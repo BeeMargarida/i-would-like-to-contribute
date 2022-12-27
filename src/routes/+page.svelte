3aceb48e7069cb3f7f29482c6d0122c2fd7c7881
Margarida Silva <ana.silva@replai.io>
2022-12-27 18:33:54 +0000

    feat: show error message and improve style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import {
+		Alert,
+		Anchor,
 		Badge,
 		Button,
 		Card,
@@ -19,20 +21,20 @@
 	import { searchProjects, type Repo } from '$lib/search';
 	import { LANGUAGES } from '$lib/languages';
 
+	const MAX_TOPICS = 3;
+
 	let loading = false;
 	let topic = '';
-	let language = '';
+	let language = 'JavaScript';
 	let licenses: string[] = ['mit', 'apache-2.0', 'gpl'];
 
 	let reposPromise: Promise<{ items: Repo[]; total_count: number }>;
-	let repos: Repo[] = [];
-	let count = 0;
 
 	async function search() {
 		loading = true;
 		try {
 			reposPromise = searchProjects({
-				topic: [topic],
+				topic: topic ? [topic] : undefined,
 				stars: '2..200',
 				forks: '2..200',
 				followers: '2..200',
@@ -41,19 +43,10 @@
 				readme: ['contributing'],
 				language: [language]
 			});
-			const response = await reposPromise;
-			repos = response.items;
-			count = response.total_count;
-		} catch (err) {
-			// @TODO: show error in pretty way - specifically the one with the requests per minute
-			console.log('err', err);
+			await reposPromise;
 		} finally {
 			loading = false;
 		}
-	}
-
-	function onCardClick(repo: Repo) {
-		window.open(repo.url, '_blank');
 	}
 </script>
 
@@ -88,53 +81,73 @@
 			</InputWrapper>
 		</Group>
 		<Center override={{ marginTop: '2rem' }}>
-			<Button size="md" variant="gradient" ripple {loading} on:click={() => search()}>
+			<Button size="md" variant="gradient" ripple disabled={loading} on:click={() => search()}>
 				Find me something!
 			</Button>
 		</Center>
 	</Card>
-	{#await reposPromise}
-		<SimpleGrid
-			cols={4}
-			breakpoints={[
-				{ maxWidth: 980, cols: 3, spacing: 'md' },
-				{ maxWidth: 755, cols: 2, spacing: 'sm' },
-				{ maxWidth: 600, cols: 1, spacing: 'sm' }
-			]}
-		>
-			{#each Array.from(Array(10).keys()) as _i (_i)}
-				<Card shadow="sm" p="lg">
-					<Skeleton height={15} radius="sm" />
-					<Skeleton height={40} radius="sm" override={{ marginTop: '1rem' }} />
-					<Skeleton height={20} radius="sm" override={{ marginTop: '1rem' }} />
-				</Card>
-			{/each}
-		</SimpleGrid>
-	{:then}
-		<SimpleGrid
-			cols={4}
-			breakpoints={[
-				{ maxWidth: 980, cols: 3, spacing: 'md' },
-				{ maxWidth: 755, cols: 2, spacing: 'sm' },
-				{ maxWidth: 600, cols: 1, spacing: 'sm' }
-			]}
-		>
-			{#each repos as repo (repo.id)}
-				<Card shadow="sm" p="lg" on:click={() => onCardClick(repo)}>
-					<Stack>
-						<Text weight={'bold'}>{repo.name}</Text>
-						<Text size="sm">{repo.description}</Text>
-						<Group position="apart">
-							<Badge color="yellow" variant="light">{repo.stargazers_count} stars</Badge>
-							<Badge color="blue" variant="light">{repo.forks_count} forks</Badge>
-						</Group>
-						<Badge color="gray" variant="light">{repo.license.name}</Badge>
-					</Stack>
-				</Card>
-			{/each}
-		</SimpleGrid>
-	{:catch error}
-		<!-- @TODO: prettify this -->
-		<p style="color: red">{error.message}</p>
-	{/await}
+	{#if reposPromise}
+		{#await reposPromise}
+			<SimpleGrid
+				cols={3}
+				breakpoints={[
+					{ maxWidth: 980, cols: 3, spacing: 'md' },
+					{ maxWidth: 755, cols: 2, spacing: 'sm' },
+					{ maxWidth: 600, cols: 1, spacing: 'sm' }
+				]}
+			>
+				{#each Array.from(Array(10).keys()) as _i (_i)}
+					<Card shadow="sm" p="lg">
+						<Skeleton height={15} radius="sm" />
+						<Skeleton height={40} radius="sm" override={{ marginTop: '1rem' }} />
+						<Skeleton height={20} radius="sm" override={{ marginTop: '1rem' }} />
+					</Card>
+				{/each}
+			</SimpleGrid>
+		{:then response}
+			<SimpleGrid
+				cols={3}
+				breakpoints={[
+					{ maxWidth: 980, cols: 3, spacing: 'md' },
+					{ maxWidth: 755, cols: 2, spacing: 'sm' },
+					{ maxWidth: 600, cols: 1, spacing: 'sm' }
+				]}
+			>
+				{#each response.items as repo (repo.id)}
+					<Card shadow="sm" p="lg" withBorder>
+						<Stack>
+							<Anchor href={repo.html_url} external weight={'bold'}>{repo.name}</Anchor>
+							<Text size="sm" lineClamp={4} override={{ lineHeight: '16px' }}
+								>{repo.description}</Text
+							>
+							<Group>
+								{#each repo.topics.slice(0, MAX_TOPICS) as topic (topic)}
+									<Badge size="sm" radius="sm" color="blue" variant="light">{topic}</Badge>
+								{/each}
+							</Group>
+							<Group position="apart">
+								<Badge size="sm" radius="sm" color="yellow" variant="light"
+									>{repo.stargazers_count} stars</Badge
+								>
+								<Badge size="sm" radius="sm" color="orange" variant="light"
+									>{repo.forks_count} forks</Badge
+								>
+							</Group>
+						</Stack>
+					</Card>
+				{/each}
+			</SimpleGrid>
+		{:catch error}
+			<Alert title="Oopsie!">
+				{#if error.message.includes('limit')}
+					<Text
+						>The maximum number of requests per minute was exceeded, please try again in the next
+						minute.</Text
+					>
+				{:else}
+					<Text>An error occurred, please try again later.</Text>
+				{/if}
+			</Alert>
+		{/await}
+	{/if}
 </Container>
