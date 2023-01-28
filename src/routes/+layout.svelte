7e3894883f743ff9686fd312d7809e3e0f63d50d
Margarida Silva <ams-amargarida@hotmail.com>
2023-01-28 11:36:39 +0000

    feat: change default style bg colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
-	import { colorScheme, ActionIcon, SvelteUIProvider } from '@svelteuidev/core';
+	import { colorScheme, dark, globalCss, ActionIcon, SvelteUIProvider } from '@svelteuidev/core';
 	import Sun from '$lib/icons/Sun.svelte';
 	import Moon from '$lib/icons/Moon.svelte';
+
+	const globalStyles = globalCss({
+		body: {
+			[`${dark.selector} &`]: {
+				backgroundColor: '$dark400',
+				color: '$dark50'
+			},
+			backgroundColor: '$blue50',
+			color: '$black'
+		}
+	});
+	globalStyles();
 
 	function toggleTheme() {
 		colorScheme.update((v) => (v === 'light' ? 'dark' : 'light'));
 	}
 </script>
 
-<SvelteUIProvider themeObserver={$colorScheme} withGlobalStyles withNormalizeCSS>
+<SvelteUIProvider themeObserver={$colorScheme} withNormalizeCSS>
 	<ActionIcon
 		override={{ position: 'absolute', top: 0, right: 0, margin: '1rem' }}
 		variant="default"
